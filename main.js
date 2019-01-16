03e51b8acf510c5571ed08433aff3d2286fe42ab
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-16 14:37:24 -0800

    added final problem

--- main.js
+++ main.js
@@ -160,3 +160,75 @@ const objectA = {
   }
 
   //console.log(validateKeys(objectB, expectedKeys));
+
+  const cipher = {
+    a: 2,
+    b: 3,
+    c: 4,
+    d: 5
+  };
+  
+  function decode(word){
+      let result = '';
+      let strArr = word.split(' ');
+      let arr = [];
+    for (let i = 0; i < strArr.length; i++){
+      for (let key in cipher){
+        let letter = strArr[i].charAt(0);
+        let newLetter = '';
+        if (key === letter){
+          let number = cipher[key];
+          let newLetter = (strArr[i].charAt(number -1));
+          arr.push(newLetter);
+              }
+      }
+    }
+    return arr.join('');
+  }
+  console.log(decode('craft block argon meter bells brown croon droop'));
+
+  function createACharacter(Name, Nickname, Race, Origin, Attack, Defense, Weapon){
+    return {
+      Name,
+      Nickname,
+      Race,
+      Origin,
+      Attack,
+      Defense,
+      Weapon,
+      describe: function(){
+        console.log(`${this.Name} is a ${this.Race} from ${this.Origin}`)
+      },
+      evaluateFight: function(character){
+        return `Your opponent takes ${this.Attack - character.Defense} damage and you receive ${character.Attack - this.Defense} damage.`
+      },
+      weapon: function(){
+        console.log(`${this.Name} is a ${this.Race} from ${this.Origin} who uses a ${this.Weapon}.`)
+      }
+    }
+  }
+
+  const characters = [
+    createACharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6), 
+    createACharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+    createACharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+    createACharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8),
+    createACharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5)
+];
+characters.push(createACharacter('Arwen', 'ar', 'Elf', 'Undomiel', 5, 8));
+
+// characters.find(function(x){
+//   return x['Nickname'] === 'aragorn';
+// }).describe();
+
+let hobbitArr = characters.filter(function(x){
+  return x.Race === 'Hobbit';
+});
+
+//console.log(attackArr);
+
+let attackArr = characters.filter(function(x){
+  return x.Attack > 5;
+});
+console.log(attackArr);
+
